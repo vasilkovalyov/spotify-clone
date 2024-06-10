7fc186ac5178bcd0647cacc0bc4665f3dc87da6f
vasilkovalyov <vasilkovalyov@gmail.com>
2024-06-10 11:16:45 +0200

    create icon component

---
+++ src/components/ui/icon/icon.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Icon from './icon';
+import { IconEnum } from './icon.type';
+
+import { model } from './icon.model';
+
+const meta = {
+  title: 'UI/Icon',
+  component: Icon,
+  parameters: {
+    layout: 'padded',
+  },
+
+  argTypes: {
+    size: {
+      control: { type: 'number' },
+    },
+    icon: {
+      options: model,
+      control: { type: 'select' },
+    },
+  },
+  args: {},
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+export const Primary: Story = {
+  args: {
+    size: 16,
+    icon: IconEnum.HOME,
+  },
+  render: (args) => <Icon {...args} />,
+};
